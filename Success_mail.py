daeb769b45a178f2665029a00df9a72e32424e42
Harasis Singh <51106510+Harasis@users.noreply.github.com>
2020-05-24 04:04:36 +0530

    Update Success_mail.py

--- Success_mail.py
+++ Success_mail.py
@@ -1,23 +1,17 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-host_address = "***********.com"
-host_pass = "*************"
-guest_address = "***************@gmail.com"
-subject = "Regarding Success of your model "
-content = '''Hello, 
-				Developer this is an email regarding to your last commit. Your last commit was taken into consideration and based on that the trained model has given best accuracy .
-				Congratulations on your success.
-			THANK YOU ...'''
-message = MIMEMultipart()
-message['From'] = host_address
-message['To'] = guest_address
-message['Subject'] = subject
-message.attach(MIMEText(content, 'plain'))
-session = smtplib.SMTP('smtp.gmail.com', 587)
-session.starttls()
-session.login(host_address, host_pass)
-text = message.as_string()
-session.sendmail(host_address, guest_address  , text)
-session.quit()
-print('Successfully sent your mail')
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")
+message = """\
+Subject: "Model created Success full"
+
+Project is done."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
