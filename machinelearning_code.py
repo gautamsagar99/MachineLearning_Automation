6a72456305042a3de5615a0a0cf9d2e45a53e06d
Harasis Singh <51106510+Harasis@users.noreply.github.com>
2020-05-24 15:32:11 +0530

    Update machinelearning_code.py

--- machinelearning_code.py
+++ machinelearning_code.py
@@ -126,11 +126,11 @@ scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
 
-accuracy_file = open('/machinelearning/accuracy_check.txt','w')
+accuracy_file = open('/machinelearning/show_accuracy.txt','w')
 accuracy_file.write(str(scores[1]))
 accuracy_file.close()
 
-display_matter = open('/machinelearning/display.html','r+')
+display_matter = open('/machinelearning/show_output.html','r+')
 display_matter.read()
 display_matter.write('<pre>\n---------------------------------------------\n')
 display_matter.write(this_layer)
