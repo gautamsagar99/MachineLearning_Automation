beb35161939983d58a5ad580a2407ebdccd33e08
Gautam Sagar <47146980+gautamsagar99@users.noreply.github.com>
2022-05-30 20:27:32 +0530

    Update checking.py

--- checking.py
+++ checking.py
@@ -2,6 +2,7 @@
 programfile = open('/root/Desktop/ml-automation/machinelearning_code.py','r')	#connecting to the code file
 code = programfile.read()				#reading the code file
 
+
 if 'keras' or 'tensorflow' in code:			#because keras or tensorflow keyword is a must for a deep learning program
 	if 'Conv2D' in code:				#beacuse if a code is of CNN conv2D layer is a must 
 		print('OK')
